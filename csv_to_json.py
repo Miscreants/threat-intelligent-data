a553ad540d3f4a4516e10da1390442f6ac83f6c6
edwardqiu <edwardqiu@users.noreply.github.com>
2020-01-17 14:26:05 -0500

    Testing multiple methods

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,4 +1,33 @@
 import csv
+import tabula
+import PyPDF2 
+import textract
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+def parse_pdf():
+	# tabula.convert_into("A_Threat_Actor_Encyclopedia.pdf", "output.csv", output_format="csv", pages='all')
+	# pdfFileObj = open('A_Threat_Actor_Encyclopedia.pdf', 'rb') 
+	# pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
+	# for i in range(0, pdfReader.numPages):
+		# print(pdfReader.getPage(i).extractText())
+	# pdfFileObj.close() 	
+	# tabula.convert_into("A_Threat_Actor_Encyclopedia.pdf", "output.json", output_format="json", pages='all') #this method has promise
+	df = tabula.read_pdf('A_Threat_Actor_Encyclopedia.pdf', pages='7', multiple_tables=True)
+	foundNameSection = False
+	for line in df:
+		for i, row in line.iterrows():
+			print(row[0], row[1])
+
+	# for line in df:
+	# 	if line == 'Names':
+	# 		foundNameSection = True
+	# 	elif foundNameSection:
+	# 		print(line)
+	# 		if line == 'Country':
+	# 			foundNameSection = False
+	# print(df)
+	# tabula.convert_into("A_Threat_Actor_Encyclopedia.pdf", "output.json", output_format="json", pages='all') #this method has promise
 
 
 def convert_csv_to_json():
@@ -8,7 +37,7 @@ def convert_csv_to_json():
         names = []
         for row in csv_reader:
             if in_name_section:
-                if row[0] == 'Country' or row[1] == 'Country':
+                if row[0] == 'Country':
                     in_name_section = False
                 else:
                     if row[1] != "":
@@ -23,4 +52,6 @@ def convert_csv_to_json():
         print(names)
 
 if __name__ == '__main__':
-    convert_csv_to_json()
+	parse_pdf()
+    # convert_csv_to_json()
+
